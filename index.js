78e73a1b43c96d55544aaf922d1ed4ac932ce942
Roxanne Desmeules <roxanne.desmeules@gmail.com>
2021-02-19 23:26:23 -0500

    attach click listener when color changes

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ class Icons {
 			this._setDarkTheme();
 		}
 
+		this.addCopyListener();
+
 		//	update icon to show correct colour
 		this.$colorContainer.find(`.column`).on('click', (ev) => {
 			this.$iconsContainer.empty();
@@ -30,64 +32,7 @@ class Icons {
 			}
 
 			this._buildIconList(color);
-		});
-
-		this.$iconsContainer.find(`.column`).on(`click`, (ev) => {
-			const $col = $(ev.target).closest('.column');
-			const $img = $col.find(`img`);
-
-			var textArea = document.createElement("textarea");
-
-			//
-			// *** This styling is an extra step which is likely not required. ***
-			//
-			// Why is it here? To ensure:
-			// 1. the element is able to have focus and selection.
-			// 2. if the element was to flash render it has minimal visual impact.
-			// 3. less flakyness with selection and copying which **might** occur if
-			//    the textarea element is not visible.
-			//
-			// The likelihood is the element won't even render, not even a
-			// flash, so some of these are just precautions. However in
-			// Internet Explorer the element is visible whilst the popup
-			// box asking the user for permission for the web page to
-			// copy to the clipboard.
-			//
-
-			// Place in the top-left corner of screen regardless of scroll position.
-			textArea.style.position = 'fixed';
-			textArea.style.top = 0;
-			textArea.style.left = 0;
-
-			// Ensure it has a small width and height. Setting to 1px / 1em
-			// doesn't work as this gives a negative w/h on some browsers.
-			textArea.style.width = '2em';
-			textArea.style.height = '2em';
-
-			// We don't need padding, reducing the size if it does flash render.
-			textArea.style.padding = 0;
-
-			// Clean up any borders.
-			textArea.style.border = 'none';
-			textArea.style.outline = 'none';
-			textArea.style.boxShadow = 'none';
-			textArea.style.overflow = 'hidden';
-			textArea.style.resize = 'none';
-			textArea.style.zIndex = '-1';
-
-			// Avoid flash of the white box if rendered for any reason.
-			textArea.style.background = 'transparent';
-
-
-			textArea.value = 'https://roxannedesmeules.github.io/notion-minimal-icons/' + $img.attr('src');
-
-			document.body.appendChild(textArea);
-			textArea.focus();
-			textArea.select();
-
-			document.execCommand('copy');
-
-			this._showCopied($col);
+			this.addCopyListener();
 		});
 	}
 
@@ -167,6 +112,66 @@ class Icons {
 			'Symbol-Dollar',
 			'WiFi',
 		];
+	}
+
+	addCopyListener() {
+		this.$iconsContainer.find(`.column`).on(`click`, (ev) => {
+			const $col = $(ev.target).closest('.column');
+			const $img = $col.find(`img`);
+
+			var textArea = document.createElement("textarea");
+
+			//
+			// *** This styling is an extra step which is likely not required. ***
+			//
+			// Why is it here? To ensure:
+			// 1. the element is able to have focus and selection.
+			// 2. if the element was to flash render it has minimal visual impact.
+			// 3. less flakyness with selection and copying which **might** occur if
+			//    the textarea element is not visible.
+			//
+			// The likelihood is the element won't even render, not even a
+			// flash, so some of these are just precautions. However in
+			// Internet Explorer the element is visible whilst the popup
+			// box asking the user for permission for the web page to
+			// copy to the clipboard.
+			//
+
+			// Place in the top-left corner of screen regardless of scroll position.
+			textArea.style.position = 'fixed';
+			textArea.style.top = 0;
+			textArea.style.left = 0;
+
+			// Ensure it has a small width and height. Setting to 1px / 1em
+			// doesn't work as this gives a negative w/h on some browsers.
+			textArea.style.width = '2em';
+			textArea.style.height = '2em';
+
+			// We don't need padding, reducing the size if it does flash render.
+			textArea.style.padding = 0;
+
+			// Clean up any borders.
+			textArea.style.border = 'none';
+			textArea.style.outline = 'none';
+			textArea.style.boxShadow = 'none';
+			textArea.style.overflow = 'hidden';
+			textArea.style.resize = 'none';
+			textArea.style.zIndex = '-1';
+
+			// Avoid flash of the white box if rendered for any reason.
+			textArea.style.background = 'transparent';
+
+
+			textArea.value = 'https://roxannedesmeules.github.io/notion-minimal-icons/' + $img.attr('src');
+
+			document.body.appendChild(textArea);
+			textArea.focus();
+			textArea.select();
+
+			document.execCommand('copy');
+
+			this._showCopied($col);
+		});
 	}
 
 	_buildIcon(color, name, forColor) {
